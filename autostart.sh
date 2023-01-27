32feef8ce97fd3709928dfd7f5c459d151859819
vbth7777 <vbth7777@gmail.com>
2023-01-28 00:16:52 +0700

    Update 1/26/2022

--- autostart.sh
+++ autostart.sh
@@ -7,6 +7,9 @@ function run {
   fi
 }
 
+#EXPORTS
+export COMMANDLINE_ARGS='--skip-torch-cuda-test'
+
 #Find out your monitor name with xrandr or arandr (save and you get this line)
 #xrandr --output VGA-1 --primary --mode 1360x768 --pos 0x0 --rotate normal
 #xrandr --output DP2 --primary --mode 1920x1080 --rate 60.00 --output LVDS1 --off &
